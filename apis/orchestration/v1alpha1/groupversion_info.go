8c6dd8b3cea00ebb6e650f30a7518edcadfb87a1
Randy Johnson <randy@qleet.io>
2023-08-09 00:30:55 -0400

    feat: add --issuer-ambient-credentials flag to cert manager deployment

--- apis/orchestration/v1alpha1/groupversion_info.go
+++ apis/orchestration/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the orchestration v1alpha1 API group
-// +kubebuilder:object:generate=true
-// +groupName=orchestration.support-services.nukleros.io
+//+kubebuilder:object:generate=true
+//+groupName=orchestration.support-services.nukleros.io
 package v1alpha1
 
 import (
