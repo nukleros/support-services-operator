44e86ac00a9eea3c89d774dc8c6928a9b3ffb02c
Randy Johnson <rj9317@gmail.com>
2023-08-10 04:36:00 -0600

    fix: update mutation logic to call correct function (#17)
    * fix: update mutation logic to call correct function
    
    * fix: update preservd-assets with changes
    
    * fix: make AppendExtraArgs private
    
    ---------
    
    Co-authored-by: Randy Johnson <randy@qleet.io>

--- apis/gateway/v1alpha1/externaldns/mutate/deployment_namespace_external_dns_route_US.go
+++ apis/gateway/v1alpha1/externaldns/mutate/deployment_namespace_external_dns_route_US.go
@@ -36,7 +36,11 @@ func MutateDeploymentNamespaceExternalDnsRoute53(
 		return []client.Object{original}, nil
 	}
 
-	// mutation logic goes here
+	mutatedObject, err := appendExtraArgs(original, parent)
+	if err != nil {
+		return []client.Object{original}, err
+	}
 
-	return []client.Object{original}, nil
+	return []client.Object{mutatedObject}, nil
+
 }
