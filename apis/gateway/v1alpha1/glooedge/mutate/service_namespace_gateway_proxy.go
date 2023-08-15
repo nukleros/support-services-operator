1fea8770890dfc38cdf78b318e8d174cf75d78ff
Randy Johnson <rj9317@gmail.com>
2023-08-14 18:07:17 -0600

    fix: add missing logic for gloo port mapping (#19)
    * fix: update mutation logic to call correct function
    
    * fix: update preservd-assets with changes
    
    * fix: make AppendExtraArgs private
    
    * fix: update gloo port mapping logic
    
    * fix: return service object instead of empty array
    
    * fix: add negative port check
    
    * fix: remove old ingress example
    
    * fix: update codegen makefile
    
    * fix: remove EXTERNAL_DNS_AWS_PREFER_CNAME=true
    
    ---------
    
    Co-authored-by: Randy Johnson <randy@qleet.io>

--- apis/gateway/v1alpha1/glooedge/mutate/service_namespace_gateway_proxy.go
+++ apis/gateway/v1alpha1/glooedge/mutate/service_namespace_gateway_proxy.go
@@ -17,9 +17,13 @@ limitations under the License.
 package mutate
 
 import (
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/nukleros/operator-builder-tools/pkg/controller/workload"
+	"github.com/nukleros/operator-builder-tools/pkg/resources"
 
 	gatewayv1alpha1 "github.com/nukleros/support-services-operator/apis/gateway/v1alpha1"
 	orchestrationv1alpha1 "github.com/nukleros/support-services-operator/apis/orchestration/v1alpha1"
@@ -36,7 +40,61 @@ func MutateServiceNamespaceGatewayProxy(
 		return []client.Object{original}, nil
 	}
 
-	// mutation logic goes here
+	// convert object to unstructured
+	service, err := resources.ToUnstructured(original)
+	if err != nil {
+		return []client.Object{original}, fmt.Errorf("failed to convert client.Object to unstructured object: %w", err)
+	}
 
-	return []client.Object{original}, nil
+	// target := unstructuredObj.DeepCopy()
+	existingPorts, found, err := unstructured.NestedSlice(service.Object, "spec", "ports")
+	if err != nil {
+		return []client.Object{original}, fmt.Errorf("failed to retrieve spec field for gateway: %w", err)
+	}
+	if !found {
+		return []client.Object{original}, fmt.Errorf("spec field not found in gateway object: %w", err)
+	}
+
+	newPorts := []interface{}{}
+	// create a port object for each port requested
+	for _, portSpec := range parent.Spec.Ports {
+		port := map[string]interface{}{}
+
+		existingPorts = removeDuplicatePorts(portSpec.Port, existingPorts)
+
+		targetPort, err := getUnprivilegedPort(portSpec.Port, parent.Spec.Ports)
+		if err != nil {
+			return []client.Object{original}, err
+		}
+
+		port["port"] = portSpec.Port
+		port["targetPort"] = targetPort
+		port["protocol"] = "TCP"
+		port["name"] = portSpec.Name
+
+		newPorts = append(newPorts, port)
+	}
+
+	newPorts = append(newPorts, existingPorts...)
+
+	err = unstructured.SetNestedSlice(service.Object, newPorts, "spec", "ports")
+	if err != nil {
+		return []client.Object{original}, fmt.Errorf("failed to set ports field for service: %w", err)
+	}
+
+	return []client.Object{service}, nil
+}
+
+// removeDuplicatePorts removes any ports that have the same port number as the one passed in.
+func removeDuplicatePorts(port int64, ports []interface{}) []interface{} {
+	deduplicated := []interface{}{}
+	for _, p := range ports {
+		if pMap, ok := p.(map[string]interface{}); ok {
+			if pMap["port"].(int64) == port {
+				continue
+			}
+		}
+		deduplicated = append(deduplicated, p)
+	}
+	return deduplicated
 }
